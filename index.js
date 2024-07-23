fbe4642e80974d23d81de8b83e7f6a6381571a62
truongtroinie <dangtruong402051@gmail.com@example.com>
2024-07-23 20:52:30 +0700

    create products

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("express-flash");
+require('dotenv').config();
+
+const database = require("./config/database");
+
+const systemConfix = require("./config/system");
+
+const route = require("./routes/client/index.route");
+
+const routeAdmin = require("./routes/admin/index.route");
+
+database.connect();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(methodOverride('_method'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
+// dùng để cài pug 
+
+// flash
+
+app.use(cookieParser("nguyet"));
+app.use(session({
+    cookie: {
+        maxAge: 60000
+    }
+}));
+app.use(flash());
+
+
+// app locals variables: tạo biến toàn cục
+app.locals.prefixAdmin = systemConfix.prefixAdmin;
+
+app.use(express.static(`${__dirname}/public`));
+
+routeAdmin(app);
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ public/admin/js/product.js
@@ -0,0 +1,50 @@
+const buttonsChangeStatus = document.querySelectorAll("[button-change-status]");
+if (buttonsChangeStatus.length > 0) {
+    const formChangeStatus = document.querySelector("#form-change-status")
+    const path = formChangeStatus.getAttribute("data-path");
+    console.log(path);
+
+    buttonsChangeStatus.forEach(button => {
+        button.addEventListener("click", () => {
+            const statusCurent = button.getAttribute("data-status");
+            const id = button.getAttribute("data-id");
+
+            let statusChange = statusCurent == "active" ? "inactive" : "active"
+
+            // console.log(statusChange);
+
+            const action = path + `/${statusChange}/${id}?_method=PATCH`;
+
+            // console.log(action);
+
+            formChangeStatus.action = action;
+
+            formChangeStatus.submit();
+        });
+    });
+}
+
+
+//delete item
+
+const buttonsDelete = document.querySelectorAll("[button-delete]");
+if (buttonsDelete.length > 0) {
+    const formDeleteItem = document.querySelector("#form-delete-item");
+    const path = formDeleteItem.getAttribute("data-path");
+
+    buttonsDelete.forEach(button => {
+        button.addEventListener("click", () => {
+            const isConfirm = confirm("Bạn có chắc muốn xoá sản phẩm này ?");
+
+            if (isConfirm) {
+                const id = button.getAttribute("data-id");
+
+                const action = `${path}/${id}?_method=DELETE`;
+
+                formDeleteItem.action = action;
+                formDeleteItem.submit();
+            } 
+
+        });
+    });
+}
